c74af421e7c56d167d404efb79140cb4d10562c0
Thitiwas <55071973+thitiwasttn@users.noreply.github.com>
2022-04-03 17:43:10 +0700

    up

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -48,7 +48,6 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     ballAnimationController.dispose();
   }
